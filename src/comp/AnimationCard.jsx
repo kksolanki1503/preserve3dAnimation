2b966d222111be1e9b3adff6a6002ac4d406101b
kksolanki1503 <kksolanki1503@gmail.com>
2024-06-24 09:46:51 +0530

    3d animation css

---
+++ src/comp/AnimationCard.jsx
@@ -0,0 +1,37 @@
+const AnimationCard = () => {
+  return (
+    <div className="list">
+      <div className="item">
+        <img src="/images/1.png" alt="anime" />
+      </div>
+      <div className="item">
+        <img src="/images/2.jpg" alt="anime" />
+      </div>
+      <div className="item">
+        <img src="/images/3.jpg" alt="anime" />
+      </div>
+      <div className="item">
+        <img src="/images/4.jpg" alt="anime" />
+      </div>
+      <div className="item">
+        <img src="/images/5.jpg" alt="anime" />
+      </div>
+      <div className="item">
+        <img src="/images/6.jpg" alt="anime" />
+      </div>
+      <div className="item">
+        <img src="/images/7.jpg" alt="anime" />
+      </div>
+      <div className="item">
+        <img src="/images/8.jpg" alt="anime" />
+      </div>
+      <div className="item">
+        <img src="/images/9.jpg" alt="anime" />
+      </div>
+      <div className="item">
+        <img src="/images/10.jpg" alt="anime" />
+      </div>
+    </div>
+  );
+};
+export default AnimationCard;
