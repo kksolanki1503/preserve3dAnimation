5b5d9c5c00c99daaeadaf9a61633a4c30ce6950f
kksolanki1503 <kksolanki1503@gmail.com>
2024-09-02 23:42:39 +0530

    cds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import AnimationCard from "./comp/AnimationCard";
 
 function App() {
-  return <AnimationCard />;
+  return (
+    <>
+      <AnimationCard />
+    </>
+  );
 }
 
 export default App;
